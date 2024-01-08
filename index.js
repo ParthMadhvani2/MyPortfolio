ad14db682fea531d2acf628aa2791a753e3daba7
Parth Madhvani <madhvaniparth2@gmail.com>
2024-01-08 18:20:22 +0530

    update

--- index.js
+++ index.js
@@ -48,55 +48,6 @@ document.querySelector('.hover-btn').onmousemove = (e) => {
     e.target.style.setProperty('--y', `${y}px`);
 };
 
-// Mail.js code to send mail of contact us form
-
-const firebaseConfig = {
-    // ... (your firebase configuration)
-};
-
-// initialize firebase
-firebase.initializeApp(firebaseConfig);
-
-// reference your database
-var contactFormDB = firebase.database().ref("contactForm");
-
-document.getElementById("contactForm").addEventListener("submit", submitForm);
-
-function submitForm(e) {
-    e.preventDefault();
-
-    var name = getElementVal("name");
-    var email = getElementVal("email");
-    var message = getElementVal("message");
-
-    saveMessages(name, email, message);
-
-    // enable alert
-    document.querySelector(".alert").style.display = "block";
-
-    // remove the alert
-    setTimeout(() => {
-        document.querySelector(".alert").style.display = "none";
-    }, 3000);
-
-    // reset the form
-    document.getElementById("contactForm").reset();
-}
-
-const saveMessages = (name, email, message) => {
-    var newContactForm = contactFormDB.push();
-
-    newContactForm.set({
-        name: name,
-        email: email,
-        message: message,
-    });
-};
-
-const getElementVal = (id) => {
-    return document.getElementById(id).value;
-};
-
 // Navbar
     document.addEventListener("DOMContentLoaded", function () {
         const menuToggle = document.querySelector('.navbar-link-toggle');
